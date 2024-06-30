aa1b88c2fd2c0feb486a5969258152a1be7fcb80
Erwin Pacua <erwin.pacua@gmail.com>
2024-06-30 18:31:22 +1200

    Add some stuff

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,29 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.mouse = ""
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+vim.opt.paste = false
+vim.opt.hlsearch = false
+vim.opt.list = true
+-- vim.opt.backspace = indent, eol, start
+vim.opt.listchars = "tab:▶ ,trail:•,precedes:❮,nbsp:.,conceal:Δ"
+vim.opt.fillchars = "vert:│,fold:-"
+vim.opt.wildmenu = true
+vim.opt.termguicolors = true
+vim.opt.title = true
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+vim.opt.cursorcolumn = false -- highlight the current column
+vim.opt.termguicolors = true
+vim.opt.laststatus = 2
+vim.opt.scrolloff = 0 -- Lines of context
+
+-- cmd("colorscheme tokyonight")
---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,16 @@
+return {
+  -- disable which-key
+  { "folke/which-key.nvim", enabled = false },
+  { "folke/trouble.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "MunifTanjim/nui.nvim", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "echasnovski/mini.ai", enabled = false },
+  { "echasnovski/mini.comment", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "hrsh7th/cmp-buffer", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+}
